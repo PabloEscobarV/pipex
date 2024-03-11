886f2c9bf73a87c4d1b92eb670b46832179e560a
Pablo Escobar <sataniv.rider@gmail.com>
2024-03-11 19:51:18 +0100

    first trying

--- pipex_cpp/pipex.cpp
+++ pipex_cpp/pipex.cpp
@@ -6,29 +6,131 @@
 /*   By: blackrider <blackrider@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 12:48:41 by blackrider        #+#    #+#             */
-/*   Updated: 2024/03/11 12:54:45 by blackrider       ###   ########.fr       */
+/*   Updated: 2024/03/11 19:47:14 by blackrider       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 extern "C"
 {
     #include <unistd.h>
+	#include "../libft/libft.h"
+	#include "../get_next_line/get_next_line_bonus.h"
 }
 #include <iostream>
 #include <cstdlib>
+#include <string>
+#include "pipex.h"
 
 using namespace std;
 
-int	main(void)
+char	**setstandartpath(char **path, const char *st_path)
+{
+	char		**tmp;
+	char		*swap_tmp;
+
+	tmp = path;
+	while (ft_strncmp(*tmp, st_path, ft_strlen(st_path)))
+		++tmp;
+	if (!(*tmp))
+		return (path);
+	swap_tmp = *path;
+	*path = *tmp;
+	*tmp = swap_tmp;
+	return (path);
+}
+
+char	**pathmaker(char **envp)
+{
+	char		*envptrim;
+	char		**res;
+	char		**tmp;
+
+	if (!envp || !(*envp))
+		return (nullptr);
+	while (ft_strncmp(*envp, PATH, ft_strlen(PATH)))
+		++envp;
+	envptrim = ft_strtrim(*envp, PATH);
+	res = ft_split(envptrim, ':');
+	if (!res)
+		return (res);
+	tmp = res;
+	while (*tmp)
+	{
+		*tmp = ft_straddchrfree(*tmp, '/');
+		++tmp;
+	}
+	free(envptrim);
+	return (setstandartpath(res, STANDARTPATH));
+}
+
+char	*findexecut(char **path, const char *execute)
+{
+	char	*fullpath;
+
+	fullpath = ft_strjoin(*path, execute);
+	while (*path && access(fullpath, F_OK))
+	{
+		free(fullpath);
+		++path;
+		fullpath = ft_strjoin(*path, execute);
+	}
+	return (fullpath);
+}
+
+int	childproc(char **argv, char **envp)
+{
+	char	**path;
+	char	*execute;
+
+	path = pathmaker(envp);
+	execute = findexecut(path, *(argv + 1));
+	cout << "EXECUTE VAR:\t" << execute << endl;
+	if (execve(execute, argv + 1, NULL) == -1)
+	{
+		perror("execve");
+		cout << "ERROR!!!\n";
+		return (-1);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv, char **envp)
 {
 	pid_t	pid;
-	int		pipepid[2];
+	int		pipefd[2];
+	char	*data;
+	char	**path;
+	string	str;
 
-	if (pipe(pipepid) == -1)
+	if (pipe(pipefd) == -1)
 	{
 		cout << "Bad chanal allocation!!!\n";
 		exit(EXIT_FAILURE);
 	}
-
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	if (!pid)
+	{
+		if (childproc(argv, envp))
+			exit(EXIT_FAILURE);
+		exit(EXIT_SUCCESS);
+	}
+	if (pid)
+	{
+		close(pipefd[1]);
+		data = get_next_line(pipefd[0]);
+		while (data)
+		{
+			str += data;
+			data = get_next_line(pipefd[0]);
+		}
+		cout << "RESULT:\n" << str << endl;
+		close(pipefd[0]);
+		exit(EXIT_SUCCESS);
+	}
 	return (0);
 }
