ada7ffdaf8125a32123b787fabf220407938f630
Pablo Escobar <sataniv.rider@gmail.com>
2024-03-13 18:05:17 +0100

    v0.2

--- src/cmdmaker.c
+++ src/cmdmaker.c
@@ -6,7 +6,7 @@
 /*   By: blackrider <blackrider@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 14:55:06 by blackrider        #+#    #+#             */
-/*   Updated: 2024/03/13 15:58:16 by blackrider       ###   ########.fr       */
+/*   Updated: 2024/03/13 18:04:59 by blackrider       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,41 +24,37 @@ int *freepipefd(int *pipefd, int fd)
     return (NULL);
 }
 
-int *pipefd_m(int flag, char **filename)
+int	startproc(char **cmd, char *pathname, char *filename, int *pipefd)
 {
-    int *pipefd;
-    int file;
+	int		file;
 
-    pipefd = malloc(2 * sizeof(int));
-    file = -2;
-    if (pipe(pipefd) == -1)
-        return (NULL);
-    if (!(*(filename + 1)))
-    {
-        file = open(*filename, O_WRONLY);
-        if (dup2(pipefd[1], file) == -1)
-            return (freepipefd(pipefd, file));
-    }
-    if (!flag)
-    {
-        file = open(*filename, O_RDONLY);
-        if (dup2(pipefd[0], file) == -1)
-            return (freepipefd(pipefd, file));
-    }
-    if (file > 0)
-        close(file);
-    return (pipefd);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (-1);
+	dup2(pipefd[1], STDOUT_FILENO);
+	dup2(file, STDIN_FILENO);
+	close(file);
+	close(pipefd[0]);
+	close(pipefd[1]);
+	execve(pathname, cmd, NULL);
+	perror("execve");
+	exit(EXIT_FAILURE);
 }
+
+
 
 int	cmdexecute(int argc, char **argv, char **envp, int v_read)
 {
-	int	    file;
-    int     *pipefd;
     pid_t   pid;
+    char    *pathname;
+    int     *pipefd;
+    char	**cmd;
 
-    pipefd = pipefd_m(argc, argv + 1);
+    pipefd = pipefd_f(v_read, argv + 1);
     if (!pipefd)
         return (-1);
+	cmd = ft_split(*argv, ' ');
+	pathname = pathexec(cmd, NULL, 0);
     pid = fork();
     if (pid == -1)
         return (-1);
